28801c0c36587270e5fd18a14781647dad79ded4
Malinda-Rathnayaka <rathnayakamalinda14@gmail.com>
2026-08-06 00:57:42 +0530

    static and dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -9,8 +9,7 @@ interface User {
 const UsersPage = async () => {
 
  const res = await fetch(
-  'https://jsonplaceholder.typicode.com/users',
-  {next: { revalidate: 10}});
+  'https://jsonplaceholder.typicode.com/users',{cache: 'no-store'});
  const users:User[] =  await res.json();
 
 
@@ -18,6 +17,7 @@ const UsersPage = async () => {
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map(user => <li key={user.id}>{user.name}</li>)}
       </ul>
